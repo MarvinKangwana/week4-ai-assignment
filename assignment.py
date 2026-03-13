4b1aab9e3a4e84ab2c8486accf757641bcd6201d
Marvin Kangwana <kangwanamarvin@gmail.com>
2026-03-13 07:42:13 +0300

    python code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,22 @@
+#Question 4
+greeting="Hello AI students"
+print(greeting)
+
+#Question 5
+name = "Marvin"
+age = 21
+city = "Nairobi"
+print("Name:" + name, "Age:" + str(age), "City:" + city)
+
+#Question 6
+programmingLanguages=["python", "java", "javascript", "C", "C++"]
+print(programmingLanguages[0])
+
+#Question 7
+studentInfo={
+  "Name":"Marvin",
+  "Age":21 ,
+  "Course":"AI"
+}
+print(studentInfo["Name"])
 
